bc3687d55a8d81015b774d1d976c8c8f8c46c40e
Ramesh Jha <ramesh@rameshjha.com>
2015-04-23 14:32:22 +0530

    removing active record stuff

--- lib/email_verifier.rb
+++ lib/email_verifier.rb
@@ -4,16 +4,16 @@ module EmailVerifier
   require 'email_verifier/checker'
   require 'email_verifier/exceptions'
   require 'email_verifier/config'
-  require "email_verifier/validates_email_realness"
+  # require "email_verifier/validates_email_realness"
 
-  if defined?(Rails::Railtie)
-    class Railtie < ::Rails::Railtie #:nodoc:
-      initializer 'rails-i18n' do |app|
-        I18n.load_path << Dir[File.join(File.expand_path(File.dirname(__FILE__) + '/../locales'), '*.yml')]
-        I18n.load_path.flatten!
-      end
-    end
-  end
+  # if defined?(Rails::Railtie)
+  #   class Railtie < ::Rails::Railtie #:nodoc:
+  #     initializer 'rails-i18n' do |app|
+  #       I18n.load_path << Dir[File.join(File.expand_path(File.dirname(__FILE__) + '/../locales'), '*.yml')]
+  #       I18n.load_path.flatten!
+  #     end
+  #   end
+  # end
 
   def self.check(email)
     v = EmailVerifier::Checker.new(email)
