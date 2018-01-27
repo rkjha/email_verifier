d74957d9c1c05f77af0cabe6516cbb6dc6532fa2
Ramesh Jha <rj3590@gmail.com>
2018-01-27 12:13:30 +0530

    update config

--- lib/email_verifier/config.rb
+++ lib/email_verifier/config.rb
@@ -4,7 +4,7 @@ module EmailVerifier
       attr_accessor :verifier_email
 
       def reset
-        @verifier_email = "nobody@nonexistant.com"
+        @verifier_email = "admin@email-checker.net"
       end
     end
     self.reset
